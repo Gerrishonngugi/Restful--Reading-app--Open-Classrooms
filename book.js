32b0ea61eff896f00316dbd4c4c33fe29ae400d7
Gerrishonngugi <gerrishonngugi@gmail.com>
2022-09-21 17:18:18 +0300

    Updated Book.js

--- book.js
+++ book.js
@@ -1,33 +1,42 @@
-class Book {
-    constructor(title, author, pages, description, read, {currentPage = 1} = {}) {
-
-        this.title = title;
-        this.author = author;
-        this.pages = Number(pages);
-        this.description = description;
-        this.read = read; 
-        this.currentPage =currentPage;
-
-        console.log(`'Created a Book via class : ${title} by ${author}'`)
-  
-  
-  }
-
-readBook(page) {
-    if (page === '' || page < 1 || page > this.pages) {
-        return console.warn('page cannot be empty or negative or exceed the total page numbers in the book');
-
-    } else if (page == this.pages) {
-        this.currentPage = page;
-        this.read = true; 
-
-    } else {
-        this.currentPage = page;
-        console.log(`I'm currently reading page ${this.currentPage} of the book titled ${this.title} by ${this.author}`);
+export class Book {
+    constructor (title, author, description, pages) {
+      this.title =title;
+      this.author =author;
+      this.description = description;
+      this.pages = pages;
+      this.currentPage = 1;
+      this.read = false;
+ 
     }
-  }
-
-}
+ 
+    readBook(page) {
+ 
+      if (page < 1 || page > this.pages) {
+        return 0;
+ 
+      } else if (page >= 1 && page < this.pages) {
+        this.currentPage = page;
+        return 1;
+ 
+      } else if(page === this.pages) {
+        this.currentPage = page;
+        this.read = true;
+        return 1;
+      }
+ 
+    }
+ 
+ }
+ 
+ export const books = [
+ 
+   new Book("Things Fall Apart", "Chinua Achebe", "A book you should read", 250, 110),
+ 
+   new Book("The Innocent Man", "John Grisham", "Chilling because its true", 400, 10),
+ 
+   new Book("He Came to Set the Captives Free", "Rebecca Brown", "spiritual warfare", 200, 72)]; 
+ 
+   
 
 
 
